aadfb0d824d89ab8de7152015e646ba49c42c5c9
Wilson Desimini <wdesimini8@gmail.com>
2022-11-02 18:07:41 -0700

    add auth request struct

---
+++ Sources/SamsungRemote/SRAuthRequest.swift
@@ -0,0 +1,13 @@
+//
+//  SRAuthRequest.swift
+//
+//
+//  Created by Wilson Desimini on 11/2/22.
+//
+
+public struct SRAuthRequest {
+    public let app: String
+    public let ipAddress: String
+    public let port: Int
+    public let scheme: String
+}
