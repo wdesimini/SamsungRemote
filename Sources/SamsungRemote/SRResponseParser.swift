87c7f37683a46fd830c2736337b899f00c3753a0
Wilson Desimini <wdesimini8@gmail.com>
2022-11-08 21:31:06 -0800

    account for unexpected auth responses

--- Sources/SamsungRemote/SRResponseParser.swift
+++ Sources/SamsungRemote/SRResponseParser.swift
@@ -13,11 +13,20 @@ public struct SRResponseParser {
     public func response<Body: Decodable>(
         from data: Data
     ) throws -> SRResponse<Body> {
-        let response: SRResponse<Body> = try decoder
-            .decode(SRResponse<Body>.self, from: data)
-        guard response.data != nil else {
-            throw SRError.channelEvent(response.event)
+        if let authResponse: SRAuthRequest.Response = try? decoder
+            .decode(SRAuthRequest.Response.self, from: data),
+            let token = authResponse.data?.token,
+           SRResponse<Body>.self != SRAuthRequest.Response.self {
+            throw SRError.unexpectedAuthorization(token: token)
+        } else if let response: SRResponse<Body> = try? decoder
+            .decode(SRResponse<Body>.self, from: data) {
+            if response.data != nil {
+                return response
+            } else {
+                throw SRError.channelEvent(response.event)
+            }
+        } else {
+            throw SRError.invalidResponse
         }
-        return response
     }
 }
